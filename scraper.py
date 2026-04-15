4c11bf7196366268739d1e58a4310aea65f8a546
LifeDesignCA <78273880+LifeDesignCA@users.noreply.github.com>
2026-04-15 11:01:06 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ import time
 
 import requests
 from bs4 import BeautifulSoup
-
+from urllib.parse import urljoin
 # ── Constantes communes ───────────────────────────────────────────────────────
 
 HEADERS = {
